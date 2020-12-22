28c00f2040f569300f4acb6274c9455fd511a2bf
Nahuel Dallacamina <nahudalla@gmail.com>
2020-12-22 20:39:53 -0300

    Implement first version of the landing

--- src/modules/MainApp/components/Button/index.js
+++ src/modules/MainApp/components/Button/index.js
@@ -1,21 +1,40 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import classnames from 'classnames'
+import { isString } from 'lodash'
 
 import classes from './styles.module.scss'
 
-const Button = ({ type = 'button', variant = 'primary', className, children, ...props }) => (
-  <button
-    type={type}
-    className={classnames(classes.button, classes[variant], className)}
-    {...props}
-  >
-    {children}
-  </button>
-)
+function Button ({
+  as: ButtonComponent = 'button',
+  variant = 'primary',
+  type,
+  className,
+  children,
+  ...props
+}) {
+  if (ButtonComponent === 'button') type ??= 'button'
+
+  className = classnames(
+    classes.button,
+    classes[variant],
+    className,
+    { [classes[`as_${ButtonComponent}`]]: isString(ButtonComponent) }
+  )
+
+  return (
+    <ButtonComponent
+      type={type}
+      className={className}
+      {...props}
+    >
+      {children}
+    </ButtonComponent>
+  )
+}
 
 Button.propTypes = {
-  state: PropTypes.string,
+  as: PropTypes.string,
   className: PropTypes.string,
   children: PropTypes.node.isRequired,
   type: PropTypes.string,
